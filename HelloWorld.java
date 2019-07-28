a41ba31fdee909abb54e5f83baea4dd58c71eb4c
Aleksey <9653160436@mail.ru>
2019-07-28 09:53:19 +0400

    my Homework

---
+++ HelloWorld.java
@@ -0,0 +1,38 @@
+ Том в устройстве C не имеет метки.
+ Серийный номер тома: 7C11-7B4C
+
+ Содержимое папки C:\Users\Anko\atom
+
+28.07.2019  09:40    <DIR>          .
+28.07.2019  09:40    <DIR>          ..
+28.07.2019  09:28                76 .gitignore
+28.07.2019  09:28               102 .travis.yml
+28.07.2019  09:28    <DIR>          bomberman
+28.07.2019  09:28                22 build.gradle
+28.07.2019  09:26    <DIR>          config
+28.07.2019  09:26    <DIR>          db_hackaton
+28.07.2019  09:26    <DIR>          gradle
+28.07.2019  09:26             5 468 gradlew
+28.07.2019  09:26             2 260 gradlew.bat
+28.07.2019  09:40                 0 HelloWorld.java
+28.07.2019  09:26    <DIR>          homeworks
+28.07.2019  09:26    <DIR>          interview
+28.07.2019  09:28    <DIR>          lecture01
+28.07.2019  09:28    <DIR>          lecture02
+28.07.2019  09:28    <DIR>          lecture03
+28.07.2019  09:28    <DIR>          lecture04
+28.07.2019  09:28    <DIR>          lecture05
+28.07.2019  09:28    <DIR>          lecture06
+28.07.2019  09:28    <DIR>          lecture07
+28.07.2019  09:28    <DIR>          lecture08
+28.07.2019  09:28    <DIR>          lecture09
+28.07.2019  09:28    <DIR>          lecture10
+28.07.2019  09:28    <DIR>          lecture11
+28.07.2019  09:28    <DIR>          lecture12
+28.07.2019  09:28             1 108 LICENCE
+28.07.2019  09:28               702 README.md
+28.07.2019  09:28                48 settings.gradle
+28.07.2019  09:28    <DIR>          tasks
+28.07.2019  09:28    <DIR>          web_hackaton
+               9 файлов          9 786 байт
+              22 папок  84 070 555 648 байт свободно
